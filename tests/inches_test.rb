c535f7d231fd7f09af42b60be3e2592c9263b96a
Phil Walker <phil1586@gmail.com>
2016-03-09 10:44:22 -0700

    Add Inches to metric + tests

--- tests/inches_test.rb
+++ tests/inches_test.rb
@@ -8,16 +8,42 @@ class InchesTest < Minitest::Test
   include ConvertableErrors
   include Inches
 
+  ###### Test Valid Input #########
   def test_error_on_invalid_input
     assert_raises(ConvertableErrors::InvalidInput) {Inches.to_centimeters("t")}
   end
 
-  def test_to_inches_int
-    assert_equal 2.54, Inches.to_centimeters(1)
+  ###### Test to_metric methods #########
+  def test_to_millimeters_int
+    assert_equal 50.8, Inches.to_millimeters(2)
   end
 
-  def test_to_inches_float
-    assert_equal 8.2296, Inches.to_centimeters(3.24)
+  def test_to_millimeters_float
+    assert_equal 82.296, Inches.to_millimeters(3.24)
+  end
+
+  def test_to_centimeters_int
+    assert_equal 5.08, Inches.to_centimeters(2)
+  end
+
+  def test_to_centimeters_float
+    assert_equal 8.229600000000001, Inches.to_centimeters(3.24)
+  end
+
+  def test_to_meters_int
+    assert_equal 0.0508, Inches.to_meters(2)
+  end
+
+  def test_to_meters_float
+    assert_equal 0.08229600000000001, Inches.to_meters(3.24)
+  end
+
+  def test_to_kilometers_int
+    assert_equal 5.08e-05, Inches.to_kilometers(2)
+  end
+
+  def test_to_kilometers_float
+    assert_equal 8.229600000000001e-05, Inches.to_kilometers(3.24)
   end
 
 end
