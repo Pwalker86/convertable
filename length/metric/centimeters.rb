16d65e2131a7c5afa0b2f58577ab88763be27dde
Phil Walker <phil1586@gmail.com>
2016-03-08 22:09:07 -0700

    Centimeters to Imperial units + test

--- length/metric/centimeters.rb
+++ length/metric/centimeters.rb
@@ -17,8 +17,25 @@ module Centimeters
     (num*0.00001).to_f.round(10)
   end
 
+  ####### to_Imperial ##########
+
   def self.to_inches(num)
     raise ConvertableErrors::InvalidInput unless num.is_a? Numeric
     (num*0.3937007874).to_f.round(10)
+  end
+
+  def self.to_feet(num)
+    raise ConvertableErrors::InvalidInput unless num.is_a? Numeric
+    (num*0.03280839895).to_f.round(10)
+  end
+
+  def self.to_yards(num)
+    raise ConvertableErrors::InvalidInput unless num.is_a? Numeric
+    (num*0.010936132983).to_f.round(10)
+  end
+
+  def self.to_miles(num)
+    raise ConvertableErrors::InvalidInput unless num.is_a? Numeric
+    (num*0.0000062137119224).to_f.round(10)
   end
 end
--- tests/centimeters_test.rb
+++ tests/centimeters_test.rb
@@ -8,20 +8,14 @@ class CentimetersTest < Minitest::Test
   include ConvertableErrors
   include Centimeters
 
+  ###### Test Valid Input #########
   def test_error_on_invalid_input
     assert_raises(ConvertableErrors::InvalidInput) {Centimeters.to_inches("t")}
   end
 
-  def test_to_inches_int
-    assert_equal 0.3937007874, Centimeters.to_inches(1)
-  end
-
-  def test_to_inches_float
-    assert_equal 1.2755905512, Centimeters.to_inches(3.24)
-  end
-
+  ###### Test to_metric methods #########
   def test_to_millimeters_int
-    assert_equal 10.0, Centimeters.to_millimeters(1)
+    assert_equal 20.0, Centimeters.to_millimeters(2)
   end
 
   def test_to_millimeters_float
@@ -29,7 +23,7 @@ class CentimetersTest < Minitest::Test
   end
 
   def test_to_meters_int
-    assert_equal 0.01, Centimeters.to_meters(1)
+    assert_equal 0.02, Centimeters.to_meters(2)
   end
 
   def test_to_meters_float
@@ -37,12 +31,44 @@ class CentimetersTest < Minitest::Test
   end
 
   def test_to_kilometers_int
-    assert_equal 0.00001, Centimeters.to_kilometers(1)
+    assert_equal 0.00002, Centimeters.to_kilometers(2)
   end
 
   def test_to_kilometers_float
     assert_equal 0.0000324, Centimeters.to_kilometers(3.24)
   end
 
+  ###### Test to_imperial methods #########
+  def test_to_inches_int
+    assert_equal 0.7874015748, Centimeters.to_inches(2)
+  end
+
+  def test_to_inches_float
+    assert_equal 1.2755905512, Centimeters.to_inches(3.24)
+  end
+
+  def test_to_feet_int
+    assert_equal 0.0656167979, Centimeters.to_feet(2)
+  end
+
+  def test_to_feet_float
+    assert_equal 0.1062992126, Centimeters.to_feet(3.24)
+  end
+
+  def test_to_yards_int
+    assert_equal 0.021872266, Centimeters.to_yards(2)
+  end
+
+  def test_to_yards_float
+    assert_equal 0.0354330709, Centimeters.to_yards(3.24)
+  end
+
+  def test_to_miles_int
+    assert_equal 1.24274e-05, Centimeters.to_miles(2)
+  end
+
+  def test_to_miles_float
+    assert_equal 2.01324e-05, Centimeters.to_miles(3.24)
+  end
 
 end
