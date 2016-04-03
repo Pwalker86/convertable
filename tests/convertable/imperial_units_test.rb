b540bc52304b911fdf60956e8447347bb1aaee88
Phil Walker <phil1586@gmail.com>
2016-04-03 16:07:58 -0600

    rename tests

---
+++ tests/convertable/imperial_units_test.rb
@@ -0,0 +1,25 @@
+require './tests/test_helper'
+
+class ImperialUnitTest < Minitest::Test
+
+  def test_method_return_class
+    assert_kind_of ConvertableUnit, 1.feet
+  end
+
+  def test_inch_method_value
+    assert_in_delta 0.082, 3.24.inches.value, 0.001
+  end
+
+  def test_foot_method_value
+    assert_in_delta 0.987, 3.24.feet.value, 0.001
+  end
+
+  def test_yard_method_value
+    assert_in_delta 2.962, 3.24.yard.value, 0.001
+  end
+
+  def test_mile_method_value
+    assert_in_delta 5214.274, 3.24.mile.value, 0.001
+  end
+
+end
